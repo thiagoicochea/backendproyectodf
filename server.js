3b4e2107bf7c639188b4ccd6a67437c1777ea4c3
ThiagoIcochea <U22330428@utp.edu.pe>
2026-05-16 09:43:06 -0500

    backend listo para deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://nendoshop.onrender.com",
     credentials: true
 }));
 
